fd2a0e8485093404185a80a9b2e43db6bc369754
Giulio Montaque <giuliom@hotmail.co.uk>
2024-03-11 23:28:09 +0000

    inital commit

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import json
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("secret_key_name")
 
 
 @app.route("/")
@@ -19,8 +21,23 @@ def about():
     return render_template("about.html", can_be_anything="About", company = data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    memberlist = {}
+    with open("data/company.json","r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                memberlist = obj
+    return render_template("member.html", member = memberlist)
+
+
+@app.route("/contact", methods=["GET","POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have got your message".format(request.form.get("name")))
+        # print(request.form)
+        # print(request.form["name"])
     return render_template("contact.html", can_be_anything="Contact")
 
 
